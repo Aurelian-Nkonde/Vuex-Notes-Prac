3a5e04e4a77365d4edc8123f972d679e8d382383
thousand10 <aurelianrobot@gmail.com>
2021-08-31 09:42:54 +0200

    pracs

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    title: 'learning core vuex',
+    heading: 'here\'s your list of fav aliens',
+    devs: [
+    ],
+  },
+  getters: {
+    devs(state){
+      return state.devs
+    }
+  },
+  mutations: {
+    datasend(state, data){
+      state.devs.push({
+        name: data.name,
+        age: data.age
+      })
+    }
+  },
+  actions: {
+    datasend({commit}, data){
+      setTimeout(() => {
+        commit('datasend', data)
+      }, 1000)
+    }
+  },
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{title}}</h1>
+
+    <form @submit.prevent="senddata">
+      <input type="text" v-model="name" placeholder="name">
+      <input type="number" v-model="age" placeholder="age">
+      <button>submit</button>
+    </form>
+
+    <hr>
+
+    <h2>{{heading}}</h2>
+    <div class="" v-if="devs.length > 0">
+      <h4>we got {{devslength}} devs</h4>
+      <br>
+      <div class="" v-for="(dev, i) in devs" :key="i">
+
+          <h1>{{ i }}:  <strong>{{ dev.name }}</strong> </h1>
+          <h2> <strong> {{ dev.age }}</strong></h2>
+
+      </div>
+    </div>
+    <div class="" v-else>
+      <p>Oops no devs</p>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import {mapState} from "vuex";
+import {mapGetters} from "vuex";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data: () => {
+    return {
+      name: "",
+      age: null
+    }
   },
+  computed: {
+    ...mapState({
+      title: "title"
+    }),
+    heading(){
+      return this.$store.state.heading
+    },
+    ...mapGetters([
+      'devs'
+    ]),
+    devslength(){
+      return this.devs.length;
+    }
+  },
+  methods: {
+    senddata(){
+      if (this.name.length > 3 && this.age != null){
+        this.$store.dispatch('datasend', {
+          name: this.name,
+          age: this.age,
+        })
+        this.name = "",
+        this.age = null
+        console.log("submiited");
+      } else {
+        alert("put enough text")
+        console.log("errors asekur");
+      }
+    }
+  }
 };
 </script>
